21a26fd34e285930dee8234d6df54bd0365fe809
Sean aye <hello@seanaye.ca>
2020-12-15 19:07:12 -0500

    add second slide

---
+++ src/slides/Slide1.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div.p-12.w-full.h-full.flex.flex-col.text-lg
+  div.text-4xl.text-center.w-full.mb-12 Venture Idea
+  div.flex.flex-col.flex-grow
+    ul.flex-grow.list-disc
+      li.w-full.mb-4 Wandr is a video sharing platform for travellers to discover and plan trips.
+      li.w-full
+        | Travel businesses can purchase user generated content
+        | for authentic marketing materials and to give back to the community.
+    ul.flex-grow.list-disc
+      li.w-full.mb-4 Travellers get value from Wandr through video reviews and trip planning features.
+      li.w-full
+        | Travel businesses have a need for user generated content,
+        | but have a difficult time acquiring it. 
+        | Wandr allows travel businesses to purchase
+        | authentic content directly from the creator.
+  
+</template>
