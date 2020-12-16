2ed20491d3f7f185e74ffa98637ee65f85d3d27b
Sean aye <hello@seanaye.ca>
2020-12-15 19:36:21 -0500

    add computed image

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
 div.w-screen.h-screen.bg-wandrdark.text-white
+  img(v-if='slideNum > 0' src='/src/assets/logo.png').absolute.top-0.right-0.w-16.m-4
   div(class='right-1/2' @click='back').absolute.left-0.top-0.bottom-0.z-top
   div(class='left-1/2' @click='forward').absolute.right-0.top-0.bottom-0.z-top
   router-view
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
 import { useRouter, useRoute } from 'vue-router'
 
 export default defineComponent({
@@ -13,24 +14,20 @@ export default defineComponent({
     const router = useRouter()
     const route = useRoute()
 
-    function getSlide () {
-      const path = route.path.replace('/', '')
-      return +path
-    }
+    const slideNum = computed(() => +route.path.replace('/', ''))
 
     function forward () {
-      const cur = getSlide()
-      router.push(`/${cur + 1}`)
+      router.push(`/${slideNum.value + 1}`)
     }
 
     function back () {
-      const cur = getSlide()
-      router.push(`/${cur - 1}`)
+      router.push(`/${slideNum.value - 1}`)
     }
 
     return {
       forward,
-      back
+      back,
+      slideNum
     }
   }
 })
