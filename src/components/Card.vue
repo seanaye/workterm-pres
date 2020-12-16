a05e04f3e7c9cb3158e76d6533f4d024cb4a4449
Sean aye <hello@seanaye.ca>
2020-12-15 21:12:12 -0500

    add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+div.rounded-2xl.bg-altgray-800.p-6.px-8
+  div.text-2xl {{ title }}
+  ul.list-disc
+    li(v-for='(item, i) in list').w-full {{ item }} 
+  slot
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String
+    },
+    list: {
+      type: Array as PropType<String[]>
+    }
+  }
+})
+</script>
