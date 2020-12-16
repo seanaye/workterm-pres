154080eca7614656bbd72acc8f8db84e7b6f62a0
Sean aye <hello@seanaye.ca>
2020-12-15 21:12:50 -0500

    update styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
@@ -9,9 +11,11 @@ module.exports = {
       },
       colors: {
         wandrdark: "#131313",
+        extradark: "#111111",
         primary: '#df2935',
         secondary: '#246eb9',
-        accent: '#ff8811'
+        accent: '#ff8811',
+        altgray: colors.coolGray
       }
     },
   },
