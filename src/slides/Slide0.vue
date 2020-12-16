a13b86e03eb7bce7ef6ac8e2b3f76276dde24472
Sean aye <hello@seanaye.ca>
2020-12-15 19:07:05 -0500

    add title slide

---
+++ src/slides/Slide0.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+div.p-12.w-full.h-full.flex.flex-col
+  div.text-4xl.text-center.w-full Eco-op End of Term Presentation
+  div.text-6xl.flex.flex-col.w-full.flex-grow.justify-center
+    div.w-full
+      img(src='/src/assets/logo.png').w-36.inline-block
+      span.font-nexa Wandr
+    div.text-3xl.w-full.mb-4.pl-3 Where to go. What to do.
+  div.w-full.pl-3 Sean Aye, 4A Geomatics
+
+</template>
