0415322a2c0770d5b43c801b377f6eae42383f6e
Sean aye <hello@seanaye.ca>
2020-12-15 19:07:23 -0500

    add router and first 2 slides

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-import { createApp } from 'vue'
+import { createApp, defineAsyncComponent } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 
+const routes: RouteRecordRaw[] = [
+  { 
+    path: '/',
+    component: defineAsyncComponent(() => import('/src/layout/Main.vue')),
+    children: [
+      { path: '', redirect: '0' },
+      { path: '0', component: defineAsyncComponent(() => import('/src/slides/Slide0.vue')) },
+      { path: '1', component: defineAsyncComponent(() => import('/src/slides/Slide1.vue'))}
+    ]
+  }
+]
 
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
